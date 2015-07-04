2f5b879be05c1fb35fa8cf54226e6266c1aabb2b
Martin Wernstål <m4rw3r@gmail.com>
2015-07-04 19:06:59 +0200

    Rendering single font glyph with foreground and background

--- src/main.rs
+++ src/main.rs
@@ -4,153 +4,199 @@ extern crate errno;
 extern crate glutin;
 #[macro_use]
 extern crate glium;
+extern crate freetype as ft;
 
 mod ctrl;
 mod pty;
 
 #[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 2],
-    color: [f32; 3],
+struct TexturedVertex {
+    xy:     [f32; 2],
+    fg_rgb: [f32; 3],
+    bg_rgb: [f32; 3],
+    st:     [f32; 2],
 }
 
-implement_vertex!(Vertex, position, color);
+implement_vertex!(TexturedVertex, xy, fg_rgb, bg_rgb, st);
 
 fn main() {
-    use clock_ticks;
     use libc;
+    use std::io;
+    
+    let (m, s) = pty::open().unwrap();
+    
+    match unsafe { libc::fork() } {
+        -1  => panic!(io::Error::last_os_error()),
+        0   => pty::run_sh(m, s),
+        pid => {
+            println!("master, child pid: {}", pid);
+           
+            window(m)
+        }
+    }
+}
+
+fn window(mut m: pty::Fd) {
+    use clock_ticks;
     use std::io;
     use std::process;
     use std::thread;
     use glium::DisplayBuild;
     use glium::index;
+    use glium::texture;
+    use glium::texture::Texture2dDataSource;
     use glium::Surface;
+    use std::borrow::Cow;
     
-    let (mut m, s) = pty::open().unwrap();
+    m.set_noblock();
     
-    match unsafe { libc::fork() } {
-        -1   => panic!(io::Error::last_os_error()),
-         0   => pty::run_sh(m, s),
-         pid => {
-            print!("master, child pid: {}\n", pid);
+    println!("Starting");
+    
+    let mut p   = ctrl::new_parser(io::BufReader::with_capacity(100, m));
+    let display = glutin::WindowBuilder::new()
+        .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 3)))
+        .build_glium()
+        .unwrap();
+    
+    println!("Attempting to load freetype");
+    let ft_lib = ft::Library::init().unwrap();
+    println!("Attempting to load font face");
+    let ft_face = ft_lib.new_face("./DejaVuSansMono/DejaVu Sans Mono for Powerline.ttf", 0).unwrap();
+    
+    println!("Attempting to set char size");
+    ft_face.set_char_size(0, 1000, 1000, 0).unwrap();
+    println!("Attempting load char");
+    ft_face.load_char('R' as usize, ft::face::RENDER).unwrap();
+    
+    let glyph_bitmap = ft_face.glyph().bitmap();
+    let tex = texture::Texture2d::new(&display, texture::RawImage2d{
+        data:   Cow::Borrowed(glyph_bitmap.buffer()),
+        width:  glyph_bitmap.width() as u32,
+        height: glyph_bitmap.rows() as u32,
+        format: texture::ClientFormat::U8
+    }.into_raw());
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, vec![
+        TexturedVertex { xy: [-0.5f32,  0.5f32], fg_rgb: [1f32, 0f32, 0f32], bg_rgb: [0.0, 0.0, 0.0], st: [0f32, 0f32] },
+        TexturedVertex { xy: [-0.5f32, -0.5f32], fg_rgb: [0f32, 1f32, 0f32], bg_rgb: [0.0, 0.0, 0.0], st: [0f32, 1f32] },
+        TexturedVertex { xy: [ 0.5f32, -0.5f32], fg_rgb: [0f32, 0f32, 1f32], bg_rgb: [0.0, 0.0, 0.0], st: [1f32, 1f32] },
+
+        TexturedVertex { xy: [ 0.5f32, -0.5f32], fg_rgb: [0f32, 0f32, 1f32], bg_rgb: [0.0, 0.0, 0.0], st: [1f32, 1f32] },
+        TexturedVertex { xy: [ 0.5f32,  0.5f32], fg_rgb: [1f32, 1f32, 1f32], bg_rgb: [0.0, 0.0, 0.0], st: [1f32, 0f32] },
+        TexturedVertex { xy: [-0.5f32,  0.5f32], fg_rgb: [1f32, 0f32, 0f32], bg_rgb: [0.0, 0.0, 0.0], st: [0f32, 0f32] },
+    ]);
+    let indices = index::NoIndices(index::PrimitiveType::TrianglesList);
+    let program = glium::Program::from_source(&display,
+        // vertex shader
+        "   #version 410
+        
+            in vec2 xy;
+            in vec3 fg_rgb;
+            in vec3 bg_rgb;
+            in vec2 st;
+
+            out vec3 pass_fg_rgb;
+            out vec3 pass_bg_rgb;
+            out vec2 pass_st;
+
+            void main() {
+                gl_Position = vec4(xy, 0, 1);
+
+                pass_fg_rgb = fg_rgb;
+                pass_bg_rgb = bg_rgb;
+                pass_st     = st;
+            }
+        ",
+        "   #version 410
+        
+            uniform sampler2D texture_diffuse;
             
-            m.set_noblock();
+            in vec3 pass_fg_rgb;
+            in vec3 pass_bg_rgb;
+            in vec2 pass_st;
             
-            
-            print!("Starting");
-            
-            let mut p   = ctrl::new_parser(io::BufReader::with_capacity(100, m));
-            let display = glutin::WindowBuilder::new().with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 3))).build_glium().unwrap();
+            out vec4 out_color;
 
-let vertex_buffer = glium::VertexBuffer::new(&display, vec![
-    Vertex { position: [-0.5, -0.5], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [ 0.0,  0.5], color: [0.0, 0.0, 1.0] },
-    Vertex { position: [ 0.5, -0.5], color: [1.0, 0.0, 0.0] },
-]);
-let indices = index::NoIndices(index::PrimitiveType::TrianglesList);
-let program = glium::Program::from_source(&display,
-    // vertex shader
-    "   #version 410
-
-        uniform mat4 matrix;
-
-        in vec2 position;
-        in vec3 color;
-
-        out vec3 v_color;
-
-        void main() {
-            gl_Position = vec4(position, 0.0, 1.0) * matrix;
-            v_color = color;
-        }
-    ",
-
-    // fragment shader
-    "   #version 410
-        in vec3 v_color;
-        out vec4 FragColor;
-
-        void main() {
-            FragColor = vec4(v_color, 1.0);
-        }
-    ",
-
-    // optional geometry shader
-    None
-).unwrap();
-let uniforms = uniform! {
-    matrix: [
-        [ 1.0, 0.0, 0.0, 0.0 ],
-        [ 0.0, 1.0, 0.0, 0.0 ],
-        [ 0.0, 0.0, 1.0, 0.0 ],
-        [ 0.0, 0.0, 0.0, 1.0 ]
-    ]
-};
-            
-            display.get_window().map(|w| w.set_title("rust_term"));
-            
-            unsafe { display.get_window().map(|w| w.make_current()); };
-            
-            let mut accumulator    = 0;
-            let mut previous_clock = clock_ticks::precise_time_ns();
-            let mut has_data       = false;
-            let mut prev_win_size  = display.get_window().and_then(|w| w.get_inner_size());
+            void main() {
+                float a = texture(texture_diffuse, pass_st).r;
+                out_color = vec4(pass_bg_rgb, 1) * (1 - a) + vec4(pass_fg_rgb, 1) * a;
+            }
+        ",
+        // optional geometry shader
+        None
+        ).unwrap();
+        
+    let uniforms = uniform! {
+        matrix: [
+            [ 1.0, 0.0, 0.0, 0.0 ],
+            [ 0.0, 1.0, 0.0, 0.0 ],
+            [ 0.0, 0.0, 1.0, 0.0 ],
+            [ 0.0, 0.0, 0.0, 1.0 ],
+        ],
+        tex: &tex,
+    };
+    
+    let params = glium::DrawParameters::new(&display);
+    
+    display.get_window().map(|w| w.set_title("rust_term"));
+    
+    unsafe { display.get_window().map(|w| w.make_current()); };
+    
+    let mut accumulator    = 0;
+    let mut previous_clock = clock_ticks::precise_time_ns();
+    let mut has_data       = false;
+    let mut prev_win_size  = display.get_window().and_then(|w| w.get_inner_size());
+    
+    loop {
+        let now = clock_ticks::precise_time_ns();
+        accumulator += now - previous_clock;
+        previous_clock = now;
+        const FIXED_TIME_STAMP: u64 = 16666667;
+        
+        while accumulator >= FIXED_TIME_STAMP {
+            accumulator -= FIXED_TIME_STAMP;
             
             loop {
-                let now = clock_ticks::precise_time_ns();
-                accumulator += now - previous_clock;
-                previous_clock = now;
-                const FIXED_TIME_STAMP: u64 = 16666667;
-                
-                while accumulator >= FIXED_TIME_STAMP {
-                    accumulator -= FIXED_TIME_STAMP;
-                    
-                    loop {
-                        match p.next() {
-                            Some(c) => {
-                                has_data = true;
-                                
-                                match c {
-                                    ctrl::Seq::SetWindowTitle(ref title) => {
-                                        display.get_window().map(|w| w.set_title(title));
-                                    },
-                                    c                                    => println!("> {:?}", c)
-                                }
+                match p.next() {
+                    Some(c) => {
+                        has_data = true;
+                        
+                        match c {
+                            ctrl::Seq::SetWindowTitle(ref title) => {
+                                display.get_window().map(|w| w.set_title(title));
                             },
-                            None    => break
+                            c                                    => println!("> {:?}", c)
                         }
-                    }
-                    
-                    if has_data {
-                    
-let mut target = display.draw();
-target.clear_color(0.0, 0.0, 0.0, 0.0);  // filling the output with the black color
-target.draw(&vertex_buffer, &indices, &program, &uniforms,
-            &std::default::Default::default()).unwrap();
-target.finish();
-                    // display.get_window().map(|w| w.swap_buffers());
-                    }
-                    
-                    for i in display.poll_events() {
-                        match i {
-                            glutin::Event::Closed => process::exit(0),
-                            _                     => println!("w {:?}", i)
-                        }
-                    }
-                    
-                    let win_size  = display.get_window().and_then(|w| w.get_inner_size());
-                    
-                    if win_size != prev_win_size {
-                        prev_win_size = win_size;
-                        has_data      = true;
-                    }
-                    else {
-                        has_data = false;
-                    }
+                    },
+                    None    => break
                 }
-                
-                thread::sleep_ms(((FIXED_TIME_STAMP - accumulator) / 1000000) as u32);
             }
-         }
+            
+            if has_data {
+                let mut target = display.draw();
+                target.clear_color(1.0, 1.0, 1.0, 1.0);
+                target.draw(&vertex_buffer, &indices, &program, &uniforms, &params).unwrap();
+                target.finish().unwrap();
+            }
+            
+            for i in display.poll_events() {
+                match i {
+                    glutin::Event::Closed => process::exit(0),
+                    _                     => println!("w {:?}", i)
+                }
+            }
+            
+            let win_size  = display.get_window().and_then(|w| w.get_inner_size());
+            
+            if win_size != prev_win_size {
+                prev_win_size = win_size;
+                has_data      = true;
+            }
+            else {
+                has_data = false;
+            }
+        }
+        
+        thread::sleep_ms(((FIXED_TIME_STAMP - accumulator) / 1000000) as u32);
     }
 }
